fa2ae5b43fb20debda5e4f454449c80f49869f15
Darshan Vekariya <darshanvekariya001@gmail.com>
2023-05-17 16:51:36 +0530

    Test

--- DataAccess/Repository/GenericRepository.cs
+++ DataAccess/Repository/GenericRepository.cs
@@ -31,7 +31,7 @@ namespace DataAccess.Repository
                 _db.SaveChanges();
             }
         }
-            
+            //Delete multiple recode
         public void DeleteInRang(IEnumerable<T> entities)
         {
             foreach (var item in entities)
